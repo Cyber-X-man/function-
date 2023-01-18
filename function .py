8f2bd7d418fc25d346fbb2f7e01123dac6cab32e
Cyber-X-man <121112632+Cyber-X-man@users.noreply.github.com>
2023-01-18 19:35:36 +0600

    Add files via upload

---
+++ function .py
@@ -0,0 +1,17 @@
+#function
+
+def add(x,y):
+    if x > y:
+        return 2
+    else:
+        return 0
+print(add(3,1))
+
+#*******************#
+
+def moon(a,b):
+    x = a + b
+    y = a - b
+    print(x, end = ", ")
+    print(y)
+moon(24,12)
